b776afec6cb7bfb7f65ceb68a37dcaeff7b3e860
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-10-15 11:45:56 +0900

    타이머와 파싱

---
+++ Busan_Air/AirData.swift
@@ -0,0 +1,15 @@
+//
+//  AirData.swift
+//  Busan_Air
+//
+//  Created by D7702_10 on 2018. 10. 15..
+//  Copyright © 2018년 ksh. All rights reserved.
+//
+
+import Foundation
+
+class AirData{
+    var dPm10 = ""
+    var dSite = ""
+    var dPm10Cai = ""
+}
